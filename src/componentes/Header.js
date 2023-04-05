487077a634466c988dd36f9da9b21360d885b15b
Your Name <you@example.com>
2023-04-06 00:10:50 +0530

    Third Commit

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -5,12 +5,12 @@ function Header() {
   return (
     <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
+      <Navbar.Brand href="#home">RESTAURANTS</Navbar.Brand>
+      {/* <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
+      </Nav> */}
     </Container>
   </Navbar>
   )
